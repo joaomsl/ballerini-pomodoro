e1520edf12ccf8fcbf29636266861a95a2fb915b
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-05 20:08:43 -0300

    installed libraries

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      timer: ['Rajdhani', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
