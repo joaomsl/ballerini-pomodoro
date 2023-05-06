f1d49a599e05a28dad73f84c7a2927dba2291943
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-06 15:36:12 -0300

    installed persist plugin for alpinejs

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,10 @@
 import Alpine from 'alpinejs'
 import tasks from './components/tasks'
+import persist from '@alpinejs/persist' 
 
 window.Alpine = Alpine
+
+Alpine.plugin(persist)
 
 Alpine.data('tasks', tasks)
 
