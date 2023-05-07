0ee0eeaa50e7ea94a5526ca3be4d7ad486ab09e4
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-07 12:29:31 -0300

    created mode and translate magic methods for alpine

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,19 @@
 import Alpine from 'alpinejs'
 import tasks from './components/tasks'
+import timer from './components/timer'
 import persist from '@alpinejs/persist' 
+
+import mode from './app/mode'
+import translate from './app/translate'
 
 window.Alpine = Alpine
 
 Alpine.plugin(persist)
 
 Alpine.data('tasks', tasks)
+Alpine.data('timer', timer)
+
+Alpine.magic('mode', () => mode)
+Alpine.magic('translate', () => translate)
 
 Alpine.start()
