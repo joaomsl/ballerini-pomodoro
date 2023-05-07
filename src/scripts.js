8418d49ce1bc302ff1112f8df80a1e7793fff5d1
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-07 16:25:23 -0300

    fixed timer component name and awaiting translator

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,21 @@
 import Alpine from 'alpinejs'
+import pomodoro from './components/pomodoro'
 import tasks from './components/tasks'
-import timer from './components/timer'
 import persist from '@alpinejs/persist' 
 
 import mode from './app/mode'
 import translate from './app/translate'
+import circularProgressBar from './components/circular-progress-bar'
 
 window.Alpine = Alpine
 
 Alpine.plugin(persist)
 
 Alpine.data('tasks', tasks)
-Alpine.data('timer', timer)
+Alpine.data('pomodoro', pomodoro)
+Alpine.data('circularProgressBar', circularProgressBar)
 
+await translate.reloadMessages() //TODO:
 Alpine.magic('mode', () => mode)
 Alpine.magic('translate', () => translate)
 
