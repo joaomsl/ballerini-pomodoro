5d66dbf9a5d744b3769e8aaacccc08a9f1fa891a
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-08 11:03:03 -0300

    refactored

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@ import pomodoro from './components/pomodoro'
 import tasks from './components/tasks'
 import persist from '@alpinejs/persist' 
 
+import app from './components/app'
 import mode from './app/mode'
 import translate from './app/translate'
 import circularProgressBar from './components/circular-progress-bar'
@@ -11,12 +12,14 @@ window.Alpine = Alpine
 
 Alpine.plugin(persist)
 
+Alpine.magic('mode', () => mode)
+Alpine.magic('translate', () => translate)
+
+Alpine.data('app', app)
 Alpine.data('tasks', tasks)
 Alpine.data('pomodoro', pomodoro)
 Alpine.data('circularProgressBar', circularProgressBar)
 
-await translate.reloadMessages() //TODO:
-Alpine.magic('mode', () => mode)
-Alpine.magic('translate', () => translate)
+await translate.reloadMessages()
 
 Alpine.start()
