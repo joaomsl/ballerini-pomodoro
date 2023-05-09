87fd97320ae0e21df5a62d0c955d1a96c4f5f11e
João Marcos <dev.joaomarcoslima@gmail.com>
2023-05-09 12:06:37 -0300

    wait content load for init application

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,13 @@
 import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist' 
 import pomodoro from './components/pomodoro'
 import tasks from './components/tasks'
-import persist from '@alpinejs/persist' 
-
 import app from './components/app'
+
 import mode from './app/mode'
 import translate from './app/translate'
 import circularProgressBar from './components/circular-progress-bar'
+import theme from './app/theme'
 
 window.Alpine = Alpine
 
@@ -14,17 +15,18 @@ Alpine.plugin(persist)
 
 Alpine.magic('mode', () => mode)
 Alpine.magic('translate', () => translate)
+Alpine.magic('theme', () => theme)
 
 Alpine.data('app', app)
 Alpine.data('tasks', tasks)
 Alpine.data('pomodoro', pomodoro)
 Alpine.data('circularProgressBar', circularProgressBar)
 
-await translate.reloadMessages()
+theme.load()
 
-const whenLoaded = () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    await translate.reloadMessages()
+  
     Alpine.start()
     document.documentElement.setAttribute('loaded', true)
-}
-
-setTimeout(whenLoaded, 300)
+})
